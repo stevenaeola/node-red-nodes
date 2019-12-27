acd995637995e558e8c5c43206a18997dfecb134
dcs0spb <s.p.bradley@durham.ac.uk>
2019-12-27 00:29:56 +0000

    pigpiod output works with pigpio-client

--- hardware/pigpiod/pi-gpiod.js
+++ hardware/pigpiod/pi-gpiod.js
@@ -18,7 +18,6 @@ module.exports = function(RED) {
     };
 
     function GPioInNode(n) {
-        console.log("Started input");
         RED.nodes.createNode(this,n);
         this.host = n.host || "127.0.0.1";
         this.port = n.port || 8888;
@@ -99,7 +98,6 @@ module.exports = function(RED) {
 
     function GPioOutNode(n) {
         RED.nodes.createNode(this,n);
-        return;
         this.host = n.host || "127.0.0.1";
         this.port = n.port || 8888;
         this.pin = n.pin;
@@ -117,7 +115,6 @@ module.exports = function(RED) {
         if (this.sermin < 5) { this.sermin = 5; }
         if (this.sermax > 25) { this.sermax = 25; }
         var node = this;
-        console.log("host is " + n.host);
         var PiGPIO;
 
         function inputlistener(msg) {
@@ -137,16 +134,17 @@ module.exports = function(RED) {
                 var pre = "";
                 if ((out >= 0) && (out <= limit)) {
                     if (RED.settings.verbose) { node.log("out: "+msg.payload); }
-                    if (!inerror) {
+                    
+                    if (node.pigpioPin && !inerror) {
                         if (node.out === "out") {
-                            PiGPIO.write(node.pin, out);
+                            node.pigpioPin.write(out);
                         }
                         if (node.out === "pwm") {
-                            PiGPIO.set_PWM_dutycycle(node.pin, parseInt(out * 2.55));
+                            node.pigpioPin.analogWrite(parseInt(out * 2.55));
                         }
                         if (node.out === "ser") {
                             var r = (node.sermax - node.sermin) * 100;
-                            PiGPIO.setServoPulsewidth(node.pin, parseInt(1500 - (r/2) + (out * r / 100)));
+                            node.pigpioPin.setServoPulsewidth(parseInt(1500 - (r/2) + (out * r / 100)));
                         }
                         node.status({fill:"green",shape:"dot",text:out.toString()});
                     }
@@ -158,21 +156,27 @@ module.exports = function(RED) {
             }
         }
 
+        var PiGPIO;
+
         if (node.pin !== undefined) {
-            PiGPIO = new Pigpio({host: this.host, post: this.port, timeout: null} );
-            var inerror = false;
-            var doit = function() {
-                PiGPIO.pi(node.host, node.port, function(err) {
-                    if (err) {
-                        node.status({fill:"red",shape:"ring",text:err.code+" "+node.host+":"+node.port});
-                        if (!inerror) { node.error(err,err); inerror = true; }
-                        node.retry = setTimeout(function() { doit(); }, 5000);
-                    }
-                    else {
+            try{
+                PiGPIO = new Pigpio.pigpio({host: this.host, post: this.port, timeout: null} );
+                const ready = new Promise((resolve, reject) => {
+                    PiGPIO.once('connected', resolve);
+                    PiGPIO.once('error', reject);
+                });
+                
+                var inerror = false;
+                var doit = async function() {
+                    try{
+                        node.pigpioPin = PiGPIO.gpio(Number(node.pin));
                         inerror = false;
-                        PiGPIO.set_mode(node.pin,PiGPIO.OUTPUT);
+                        await node.pigpioPin.modeSet('output');
+                        var err = false; // replace with catch
+                        if (err) {
+                        }
                         if (node.set) {
-                            setTimeout(function() { PiGPIO.write(node.pin,node.level); }, 25 );
+                            setTimeout(function() { node.pigpioPin.write(node.level); }, 25 );
                             node.status({fill:"green",shape:"dot",text:node.level});
                         } else {
                             node.status({fill:"green",shape:"dot",text:"node-red:common.status.ok"});
@@ -182,19 +186,25 @@ module.exports = function(RED) {
                         //     PiGPIO.set_PWM_range(1000);
                         // }
                     }
-                });
+                    catch (error) {
+                        node.status({fill:"red",shape:"ring",text:error.code+" "+node.host+":"+node.port});
+                        if (!inerror) { node.error(err,err); inerror = true; }
+                        node.retry = setTimeout(function() { doit(); }, 5000);
+                    }
+                }
+                
+                doit();
+                node.on("input", inputlistener);
             }
-            doit();
-            node.on("input", inputlistener);
+            catch(error) {
+                node.warn(RED._("pi-gpiod:errors.invalidpin")+": "+node.pio);
+            }
         }
-        else {
-            node.warn(RED._("pi-gpiod:errors.invalidpin")+": "+node.pio);
-        }
-
+        
         node.on("close", function(done) {
             if (node.retry) { clearTimeout(node.retry); }
             node.status({fill:"grey",shape:"ring",text:"pi-gpiod.status.closed"});
-            PiGPIO.close();
+            PiGPIO.end();
             done();
         });
     }
